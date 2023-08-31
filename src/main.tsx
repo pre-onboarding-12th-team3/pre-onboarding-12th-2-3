5e403bedf483e3bd06ad3047d975d6b0be629620
ShinJaeIl01 <ekvm555@naver.com>
2023-08-31 19:43:29 +0900

    [!HOTFIX] Router Import 경로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import '@/index.css';
-import router from './routes/Router';
+import router from './routes/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
