13046a14d3bb1da419b393196a5f4be6ab8dbad6
ShinJaeIl01 <ekvm555@naver.com>
2023-08-31 11:53:23 +0900

    [Chore] Prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,39 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:import/typescript",
-        "plugin:import/recommended",
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: '{react}',
+            group: 'builtin',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
     ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["react-refresh"],
-    rules: {
-        "react-refresh/only-export-components": [
-            "warn",
-            { allowConstantExport: true },
-        ],
-        "@typescript-eslint/no-unused-vars": ["warn"],
-        "import/order": [
-            "error",
-            {
-                groups: ["builtin", "external", "internal"],
-                pathGroups: [
-                    {
-                        pattern: "{react}",
-                        group: "builtin",
-                        position: "after",
-                    },
-                ],
-                pathGroupsExcludedImportTypes: ["react"],
-                alphabetize: {
-                    order: "asc",
-                    caseInsensitive: true,
-                },
-                "newlines-between": "always",
-            },
-        ],
-    },
+  },
 };
