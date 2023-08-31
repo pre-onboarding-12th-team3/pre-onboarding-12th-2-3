186f40bebfb68754a6c9fc98667f9a24dc857789
ShinJaeIl01 <ekvm555@naver.com>
2023-08-31 11:21:15 +0900

    [Chore] Eslint Import 구문 정렬 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,40 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:import/typescript",
+        "plugin:import/recommended",
     ],
-  },
-}
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh"],
+    rules: {
+        "react-refresh/only-export-components": [
+            "warn",
+            { allowConstantExport: true },
+        ],
+        "@typescript-eslint/no-unused-vars": ["warn"],
+        "import/order": [
+            "error",
+            {
+                groups: ["builtin", "external", "internal"],
+                pathGroups: [
+                    {
+                        pattern: "{react}",
+                        group: "builtin",
+                        position: "after",
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ["react"],
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true,
+                },
+                "newlines-between": "always",
+            },
+        ],
+    },
+};
