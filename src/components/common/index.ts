1c0aebba3770602eb95f9c62dd66812381870b2d
ukssss <1872003115su@gmail.com>
2023-09-01 12:25:52 +0900

    [Feat] Advertisement 컴포넌트 제작

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
+export { default as Advertisement } from './Advertisement';
 export { default as Header } from './Header';
 export { default as Heading } from './Heading';
 export { default as MarkdownViewer } from './MarkdownViewer';
