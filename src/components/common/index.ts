01e78bfef938e46a9540a52270c5ec54c1f89d0b
ukssss <1872003115su@gmail.com>
2023-09-01 12:47:06 +0900

    [Feat] Loading 컴포넌트 제작

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,5 @@
 export { default as Advertisement } from './Advertisement';
 export { default as Header } from './Header';
 export { default as Heading } from './Heading';
+export { default as Loading } from './Loading';
 export { default as MarkdownViewer } from './MarkdownViewer';
